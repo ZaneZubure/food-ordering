828c393985c0ac2ff36c1a0c126233f5aa879583
ZaneZubure <zane.rainska@gmail.com>
2022-08-26 11:53:45 +0300

    fixed logout

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,7 +1,7 @@
     <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center ">            
             <div class="px-2">
-                <a class="btn btn-primary" href="/diner" role="button">Sākums</a>
+                <a class="btn btn-primary" href="/diner" role="button">Ēdnīcas</a>
                 <a class="btn btn-success" href="/purchase" role="button">Apsktīt Pasūtījumu</a>
             </div>          
         </nav>
